494e51b09486c2013c97df5f8a8b123676dbfcde
Danny van Kooten <dannyvankooten@gmail.com>
2016-01-26 11:07:45 +0700

    Instantiate list synchronizer even if "auto-sync enabled" is set to false, to allow programmatically calling the actions.

--- bootstrap.php
+++ bootstrap.php
@@ -21,40 +21,49 @@ $GLOBALS['mailchimp_sync'] = $plugin;
 $list_synchronizer = null;
 
 // if a list was selected, initialise the ListSynchronizer class
-if( ! empty( $plugin->options['list'] ) && $plugin->options['enabled'] ) {
+if( ! empty( $plugin->options['list'] ) ) {
 
 	// instantiate synchronizer
 	$list_synchronizer = new ListSynchronizer( $plugin->options['list'], $plugin->options['role'], $plugin->options );
 	$list_synchronizer->add_hooks();
 
-	// create a job queue
-	$queue = new Queue( 'mc4wp_sync_queue' );
+	// if auto-syncing is enabled, setup queue and worker
+	if( $plugin->options['enabled'] ) {
 
-	// create a worker and have it work on "init" when doing CRON
-	$worker = new Worker( $queue, $list_synchronizer );
-	$worker->add_hooks();
+		// create a job queue
+		$queue = new Queue( 'mc4wp_sync_queue' );
 
-	if( defined( 'DOING_CRON' ) && DOING_CRON ) {
-		add_action( 'init', array( $worker, 'work' ) );
+		// create a worker and have it work on "init" when doing CRON
+		$worker = new Worker( $queue, $list_synchronizer );
+		$worker->add_hooks();
+
+		if( defined( 'DOING_CRON' ) && DOING_CRON ) {
+			add_action( 'init', array( $worker, 'work' ) );
+		}
 	}
 }
 
-// Load area-specific code
 if( ! is_admin() ) {
 
+	// public section
 	if( $list_synchronizer instanceof ListSynchronizer ) {
 		$webhook_listener = new Webhook\Listener( $list_synchronizer->meta_key, $plugin->options['field_mappers'] );
 		$webhook_listener->add_hooks();
 	}
 
 } elseif( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+
+	// ajax listeners
 	$ajax = new AjaxListener( $plugin->options );
 	$ajax->add_hooks();
 } else {
+
+	// admin screens
 	$admin = new Admin\Manager( $plugin->options, $list_synchronizer );
 	$admin->add_hooks();
 }
 
+// WP CLI Commands
 if( defined( 'WP_CLI' ) && WP_CLI ) {
 	WP_CLI::add_command( 'mailchimp-sync', 'MC4WP\\Sync\\CLI\\Command' );
 }
